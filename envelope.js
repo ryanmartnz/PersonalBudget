036dfec49ac992981f97a51c178fa66790efbbe8
ryanmartinez <martinez274@usf.edu>
2023-12-15 16:50:29 -0500

    new route to delete an envelope by id and a new route to transfer the budget from one envelope to another

--- envelope.js
+++ envelope.js
@@ -2,7 +2,7 @@ const envelopesRouter = require('express').Router();
 
 module.exports = envelopesRouter;
 
-const { envelopes, addNewEnvelope, findEnvelopeById, updateEnvelopeById } = require('./db');
+const { envelopes, addNewEnvelope, findEnvelopeById, updateEnvelopeById, deleteEnvelopeById } = require('./db');
 
 envelopesRouter.param('id', (req, res, next, id) => {
     const envelope = findEnvelopeById(id);
@@ -37,5 +37,32 @@ envelopesRouter.post('/', (req, res, next) => {
         res.status(201).send(newEnvelope);
     } catch(err) {
         res.status(400).send(err.message);
+    }
+});
+
+envelopesRouter.delete('/:id', (req, res, next) => {
+    if(deleteEnvelopeById(req.params.id)) {
+        res.status(204).send();
+    } else {
+        res.status(500).send();
+    }
+});
+
+envelopesRouter.post('/transfer/:from/:to', (req, res, next) => {
+    try {
+        const fromEnvelope = findEnvelopeById(req.params.from);
+        const toEnvelope = findEnvelopeById(req.params.to);
+        if(fromEnvelope && toEnvelope) {
+            const toEnvelopeNewBudget = Number(fromEnvelope.budget) + Number(toEnvelope.budget);
+            updateEnvelopeById(toEnvelope.id, toEnvelope.title, toEnvelopeNewBudget);
+            updateEnvelopeById(fromEnvelope.id, fromEnvelope.title, 0);
+            res.status(201).send(`Budget of \$${fromEnvelope.budget} successfully transferred from ${fromEnvelope.title} to ${toEnvelope.title}.`);
+        } else if(!fromEnvelope) {
+            res.status(404).send(`Envelope with id #${req.params.from} not found!`);
+        } else if(!toEnvelope) {
+            res.status(404).send(`Envelope with id #${req.params.to} not found!`);
+        }
+    } catch(err) {
+        res.status(500).send(err.message);
     }
 });
