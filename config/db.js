bc6aac85858b8685139f34a052ca8a8134c4aec3
ryanmartinez <martinez274@usf.edu>
2023-12-26 17:11:25 -0500

    api now stores envelope data in a postgreSQL database for retrieval and update of envelopes

--- config/db.js
+++ config/db.js
@@ -1,20 +1,41 @@
-const envelopes = [
-    {
-      id: 1,
-      title: "Rent",
-      budget: 1000
-    },
-    {
-      id: 2,
-      title: "Groceries",
-      budget: 300
-    },
-    {
-      id: 3,
-      title: "Entertainment",
-      budget: 400
-    },
-];
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'ryan',
+  host: 'localhost',
+  database: 'personalbudget',
+  password: 'jackalope',
+  port: 5432,
+});
+
+const query = async (text, params) => {
+  const start = Date.now();
+  const res = await pool.query(text, params);
+  const duration = Date.now() - start;
+  console.log('executed query', { text, duration, rows: res.rowCount });
+  return res;
+};
+
+module.exports = {
+  query
+};
+
+// const envelopes = [
+//     {
+//       id: 1,
+//       title: "Rent",
+//       budget: 1000
+//     },
+//     {
+//       id: 2,
+//       title: "Groceries",
+//       budget: 300
+//     },
+//     {
+//       id: 3,
+//       title: "Entertainment",
+//       budget: 400
+//     },
+// ];
   
-module.exports = envelopes;
+// module.exports = envelopes;
   
