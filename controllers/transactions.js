9a51d6e160f44fe7eed1968ec258707e4c8633b5
ryanmartinez <martinez274@usf.edu>
2023-12-27 16:08:40 -0500

    all queries to the database now use clients

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -3,7 +3,9 @@ const { createId, updateEnvelopeBudget } = require('../helpers/helpers');
 
 const getTransactions = async (req, res, next) => {
     try {
-        const transactions = await db.query("SELECT * FROM transactions ORDER BY date");
+        const client = await db.getClient();
+        const transactions = await client.query("SELECT * FROM transactions ORDER BY date");
+        client.release();
         return res.status(200).send(transactions.rows);
     } catch(err) {
         return res.status(500).send(err.message);
@@ -23,16 +25,18 @@ const newTransaction = async (req, res, next) => {
         if(req.envelope.budget < req.amount) {
             return res.status(400).send('Transaction amount exceeds envelope budget! Transaction rejected.');
         }
-        const results = await db.query("SELECT * FROM transactions ORDER BY id");
+        const client = await db.getClient();
+        const results = await client.query("SELECT * FROM transactions ORDER BY id");
         const newId = createId(results.rows);
-        const newTransaction = await db.query(
+        const newTransaction = await client.query(
             "INSERT INTO transactions (id, date, amount, recipient, envelope_id) VALUES ($1, $2, $3, $4, $5) RETURNING *",
             [newId, req.date, req.amount, req.recipient, req.envelope.id]
         );
+        client.release();
         
         const newEnvelopeBudget = req.envelope.budget - req.amount;
         await updateEnvelopeBudget(req.envelope, newEnvelopeBudget);
-
+        
         return res.status(201).send(newTransaction.rows[0]);
     } catch(err) {
         return res.status(500).send(err.message);
@@ -42,14 +46,13 @@ const newTransaction = async (req, res, next) => {
 const updateTransaction = async (req, res, next) => {
     try {
         let newEnvelopeBudget;
+        const client = await db.getClient();
         if(req.transaction.envelope_id !== req.envelope.id) {
             if(req.envelope.budget < req.amount) {
                 return res.status(400).send('Transaction amount exceeds new envelope budget! Transaction update rejected.');
             }
-            const originalEnvelope = (await db.query("SELECT * FROM envelopes WHERE id = $1", [req.transaction.envelope_id])).rows[0];
-            console.log(originalEnvelope.budget);
+            const originalEnvelope = (await client.query("SELECT * FROM envelopes WHERE id = $1", [req.transaction.envelope_id])).rows[0];
             const newOriginalBudget = parseFloat(originalEnvelope.budget) + req.amount;
-            console.log(newOriginalBudget);
             await updateEnvelopeBudget(originalEnvelope, newOriginalBudget);
 
             newEnvelopeBudget = req.envelope.budget - req.amount;
@@ -60,11 +63,12 @@ const updateTransaction = async (req, res, next) => {
             }
         }
 
-        const updatedTransaction = await db.query(
+        const updatedTransaction = await client.query(
             "UPDATE transactions SET date = $1, amount = $2, recipient = $3, envelope_id = $4 WHERE id = $5 RETURNING *",
             [req.date, req.amount, req.recipient, req.envelope.id, req.transaction.id]
         );
-        console.log(newEnvelopeBudget);
+        client.release();
+
         await updateEnvelopeBudget(req.envelope, newEnvelopeBudget);
 
         return res.status(201).send(updatedTransaction.rows[0]);
@@ -75,9 +79,11 @@ const updateTransaction = async (req, res, next) => {
 
 const deleteTransaction = async (req, res, next) => {
     try {
-        await db.query("DELETE FROM transactions WHERE id = $1", [req.transaction.id]);
+        const client = await db.getClient();
+        await client.query("DELETE FROM transactions WHERE id = $1", [req.transaction.id]);
 
-        const envelope = (await db.query("SELECT * FROM envelopes WHERE id = $1", [req.transaction.envelope_id])).rows[0];
+        const envelope = (await client.query("SELECT * FROM envelopes WHERE id = $1", [req.transaction.envelope_id])).rows[0];
+        client.release();
         const newEnvelopeBudget = parseFloat(envelope.budget) + req.transaction.amount;
         await updateEnvelopeBudget(envelope, newEnvelopeBudget);
 
